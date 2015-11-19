19bd007799d56349082a57a280625540174583de
Erlend Andresen <erlend.andresen@gmail.com>
2015-11-19 01:14:00 +0100

    pulled ws logic out of server.js

--- js/server/server.js
+++ js/server/server.js
@@ -1,43 +1,10 @@
-var WebSocketServer = require('websocket').server;
-var http = require('http');
 var EventEmitter = require("events").EventEmitter;
 var SUBS = require('../const/sub');
+var MyWebSocket = require('./myWebSocket.js')();
 
-var server = http.createServer(function(request, response) {
-  console.log((new Date()) + ' Received request for ' + request.url);
-  response.writeHead(404);
-  response.end();
-});
+MyWebSocket.init(onInit, onRequest);
 
-server.listen(8080, function() {
-  console.log((new Date()) + ' Server is listening on port 8080');
-});
-
-wsServer = new WebSocketServer({
-  httpServer: server,
-  autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  console.log(origin);
-  return true;
-}
-
-wsServer.on('request', function(request) {
-  if (!originIsAllowed(request.origin)) {
-    // Make sure we only accept requests from an allowed origin
-    request.reject();
-    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-    return;
-  }
-
-  var connection = request.accept('echo-protocol', request.origin);
-
-  console.log((new Date()) + ' Connection accepted.');
-
-  var ee = new EventEmitter();
-
+function onInit(ee) {
   setInterval(function () {
     console.log('emit ' + SUBS.TEAMS_CHANGED);
     ee.emit(SUBS.TEAMS_CHANGED);
@@ -47,32 +14,20 @@ wsServer.on('request', function(request) {
     console.log('emit ' + SUBS.MATCHES_CHANGED);
     ee.emit(SUBS.MATCHES_CHANGED);
   }, 1500);
+};
 
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      console.log('Received Message: ' + message.utf8Data);
-      var req = JSON.parse(message.utf8Data);
+function onRequest(ee, message, connection) {
+  console.log('Received Message: ' + message.utf8Data);
+  var req = JSON.parse(message.utf8Data);
 
-      if(req.sub === SUBS.MATCHES_CHANGED) {
-        ee.on(SUBS.MATCHES_CHANGED, function () {
-          connection.sendUTF(JSON.stringify({sub: SUBS.MATCHES_CHANGED, 'n': 'Server says matches has changed'}))
-        });
-      }
-      if(req.sub === SUBS.TEAMS_CHANGED) {
-        ee.on(SUBS.TEAMS_CHANGED, function () {
-          connection.sendUTF(JSON.stringify({sub: SUBS.TEAMS_CHANGED, teams: [{name: 'ws', id: 1}]}));
-        });
-      }
-    }
-
-    else if (message.type === 'binary') {
-      console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-      connection.sendBytes(message.binaryData);
-    }
-  });
-
-  connection.on('close', function(reasonCode, description) {
-    console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-  });
-
-});
+  if(req.sub === SUBS.MATCHES_CHANGED) {
+    ee.on(SUBS.MATCHES_CHANGED, function () {
+      connection.sendUTF(JSON.stringify({sub: SUBS.MATCHES_CHANGED, 'n': 'Server says matches has changed'}))
+    });
+  }
+  if(req.sub === SUBS.TEAMS_CHANGED) {
+    ee.on(SUBS.TEAMS_CHANGED, function () {
+      connection.sendUTF(JSON.stringify({sub: SUBS.TEAMS_CHANGED, teams: [{name: 'ws', id: 1}]}));
+    });
+  }
+};
